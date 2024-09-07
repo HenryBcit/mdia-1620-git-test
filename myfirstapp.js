277ca046e9aa36ebe17c32f2494f7226d5a2aa99
HenryBcit <hleung70@bcit.ca>
2024-09-06 22:17:29 -0700

    new changes

--- myfirstapp.js
+++ myfirstapp.js
@@ -5,3 +5,8 @@ This is a multi line comment, I can write essays here and it won't break the pro
 */
 
 console.log("Going to make some changes.");
+console.log("Nice updates");
+console.log("boolean", true);
+console.log("number", 1);
+console.log("string", "yes");
+console.log("I have", 1, "dog and that is", true);
