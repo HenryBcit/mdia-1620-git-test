b898325d9f919b774e6888fad76a77ce9655fce1
HenryBcit <hleung70@bcit.ca>
2024-09-06 22:13:58 -0700

    made new updates

--- myfirstapp.js
+++ myfirstapp.js
@@ -4,6 +4,4 @@
 This is a multi line comment, I can write essays here and it won't break the program
 */
 
-console.log("\x1b[42m", "Hi, welcome to my first program.", 
-  "\x1b[37m",
-  "My second words to you are these");
+console.log("Going to make some changes.");
