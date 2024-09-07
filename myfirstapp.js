38fafe197d7bb4a547350ac32f1fbb2a0e013b5c
HenryBcit <hleung70@bcit.ca>
2024-09-06 22:11:35 -0700

    my first file to git

---
+++ myfirstapp.js
@@ -0,0 +1,9 @@
+//This is a comment, I can write anything and it won't break the program
+
+/*
+This is a multi line comment, I can write essays here and it won't break the program
+*/
+
+console.log("\x1b[42m", "Hi, welcome to my first program.", 
+  "\x1b[37m",
+  "My second words to you are these");
